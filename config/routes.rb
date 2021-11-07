c776471a68fd056ea077a818e3cb2bc02f3edd38
Marcin <marcin.koper@ymail.com>
2021-11-07 17:29:42 +0100

    Modify users mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   resources :spots
   root 'pages#home'
   get 'pages/about'
   get 'pages/history'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
 
   post '/slack/command', to: 'slack/commands#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_141338) do
+ActiveRecord::Schema.define(version: 2021_11_07_154523) do
 
   create_table "histories", force: :cascade do |t|
     t.integer "spot_id"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_11_07_141338) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "email"
   end
 
 end
