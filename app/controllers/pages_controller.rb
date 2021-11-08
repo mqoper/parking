3555e462e05e7b1c64b46903f25ad1b39be29b37
Marcin <marcin.koper@ymail.com>
2021-11-08 14:50:12 +0100

    Change history show for user reservation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,8 @@ class PagesController < ApplicationController
 
   def history
     @histories = History.all.paginate(page: params[:page], per_page: 10)
+    @spots = Spot.all
+    @spot = @histories.distinct.pluck(:spot_id)
   end
 
   # def create
