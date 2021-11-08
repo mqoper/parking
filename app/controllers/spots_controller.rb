9c4d488b869c0febf74ef4f2fa464ad6ad1e1530
Marcin <marcin.koper@ymail.com>
2021-11-08 17:00:04 +0100

    Change Users and Spots functionality

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,41 +1,34 @@
 class SpotsController < ApplicationController
-  before_action :set_spot, only: %i[show edit update destroy]
+  before_action :set_spot, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  # before_action :require_same_user, only: [:edit, :update, :destroy]
 
-  # GET /spots or /spots.json
   def index
     @spots = Spot.all
     @users = User.all
   end
 
-  # GET /spots/1 or /spots/1.json
   def show
   end
 
-  # GET /spots/new
   def new
     @spot = Spot.new
   end
 
-  # GET /spots/1/edit
   def edit
   end
 
-  # POST /spots or /spots.json
   def create
     @spot = Spot.new(spot_params)
-
-    respond_to do |format|
-      if @spot.save
-        format.html { redirect_to @spot, notice: 'Spot was successfully created.' }
-        format.json { render :show, status: :created, location: @spot }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @spot.errors, status: :unprocessable_entity }
-      end
+    # @spot.user = current_user
+    if @spot.save
+      flash[:notice] = "Spot was created successfully."
+      redirect_to @spot
+    else
+      render 'new'
     end
   end
 
-  # PATCH/PUT /spots/1 or /spots/1.json
   def update
 
     # if @spot.update(spot_params) == true
@@ -70,7 +63,6 @@ class SpotsController < ApplicationController
     end
   end
 
-  # DELETE /spots/1 or /spots/1.json
   def destroy
     @spot.destroy
     respond_to do |format|
@@ -81,14 +73,11 @@ class SpotsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_spot
     @spot = Spot.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def spot_params
     params.require(:spot).permit(:name, :reserved)
   end
-
 end
