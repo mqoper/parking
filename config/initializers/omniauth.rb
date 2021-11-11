099207577ee29881ddf56546afddd1d291ada9f8
Marcin <marcin.koper@ymail.com>
2021-11-11 20:32:50 +0100

    Omniauth credentials correct

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack, Rails.application.config.credentials.dig(:slack, :api_key), Rails.application.config.credentials.dig(:slack, :api_secret),{
+  # provider :slack, Rails.application.config.credentials.dig(:slack, :api_key), Rails.application.config.credentials.dig(:slack, :api_secret),{
+  #   scope: 'identify',
+  #   :provider_ignores_state => true
+  # }
+
+  provider :slack, '2694475882850.2707181232417', 'a8f6374435f3f96f6efe4bac5f216c89', {
     scope: 'identify',
     :provider_ignores_state => true
   }
+
 end
